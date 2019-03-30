5ef27dad17bdceafbd9d51739959561346651d0e
BackrndSource <back.rndsource@gmail.com>
2019-03-30 04:30:02 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales['es'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'enviar un tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'acción personal',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'información RolePlay',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = 'mensaje',
+  ['generic_argument_help'] = 'el mensaje',
+  ['unknown_command'] = '^3%s^0 no es un comando válido!',
+}
